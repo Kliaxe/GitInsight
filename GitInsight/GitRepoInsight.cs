fafafe570d1c41eadb6885b20d47a7704c03536d
Daniel <midf@itu.dk>
2022-10-25 13:14:44 +0200

    Changed folder structure

---
+++ GitInsight/GitRepoInsight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitInsight
+{
+    internal class GitRepoInsight : IGitRepoInsight
+    {
+        public string GetCommitsOverTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCommitsOverTimeByUser(string user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
