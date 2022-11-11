194dff30ecac05ff207b5337acc7d828fa750de9
Silas Oler <silasmartins@outlook.dk>
2022-11-11 14:27:06 +0100

    Server tests done

--- GitInsight.Server.Tests/Controllers/GitInsightControllerTests.cs
+++ GitInsight.Server.Tests/Controllers/GitInsightControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using GitInsight.Infrastructure;
 using Microsoft.Extensions.Logging;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,23 +11,22 @@ namespace GitInsight.Server.Tests.Controllers
 {
     public class GitInsightControllerTests
     {
-        private readonly GitInsightController _sut;
+        private readonly GitInsightController _controller;
 
         private const string _gitOwner = "Lukski175";
         private const string _gitRepositoryName = "assignment-04";
-        private const string _gitUser = "Moedefeis";
 
         public GitInsightControllerTests()
         {
             var logger = Substitute.For<ILogger<GitInsightController>>();
-            _sut = new GitInsightController(logger);
+            _controller = new GitInsightController(logger);
         }
 
         [Fact]
         public async Task Get_NonExisting()
         {
             var expected = new List<DateCount>() { new DateCount(new DateTime(2022, 10, 4), 1) };
-            var result = await _sut.Get(_gitOwner, _gitRepositoryName);
+            var result = await _controller.Get(_gitOwner, _gitRepositoryName);
             result.Should().NotBeEquivalentTo(expected);
         }
 
@@ -45,37 +45,63 @@ namespace GitInsight.Server.Tests.Controllers
                 new DateCount(new DateTime(2020, 9, 10), 1),
             };
 
-            var result = await _sut.Get(_gitOwner, _gitRepositoryName);
+            var result = await _controller.Get(_gitOwner, _gitRepositoryName);
             result.Should().BeEquivalentTo(expected);
         }
 
-        //[Fact]
-        //public async Task Get_User_NonExisting()
-        //{
-        //    var expected = new List<DateCount>() { new DateCount(new DateTime(2022, 10, 4), 1) };
-        //    var result = await _sut.Get(_gitOwner, _gitRepositoryName);
-        //    result.Should().NotBeEquivalentTo(expected);
-        //}
+        [Fact]
+        public async Task Get_User_NonExisting()
+        {
+            var expected = new List<UserDateCounts>() { 
+                new UserDateCounts(new User("Test", "Test@github.com"),
+                new List<DateCount>() { new DateCount(new DateTime(0198, 10, 4), 1) })
+            };
+            var result = await _controller.Get(_gitOwner, _gitRepositoryName, "user").Result.;
+            result.Should().NotBeEquivalentTo(expected);
+        }
 
         [Fact]
         public async Task Get_User_Existing()
         {
-            var expected = new List<DateCount>()
+            var expected = new List<UserDateCounts>()
             {
-                new DateCount(new DateTime(2022, 10, 4), 9),
-                new DateCount(new DateTime(2022, 9, 30), 2),
-                new DateCount(new DateTime(2022, 9, 29), 1),
-                new DateCount(new DateTime(2022, 9, 28), 2),
-                new DateCount(new DateTime(2022, 9, 25), 1),
-                new DateCount(new DateTime(2020, 9, 23), 3),
-                new DateCount(new DateTime(2020, 9, 22), 1),
-                new DateCount(new DateTime(2020, 9, 10), 1),
+                new UserDateCounts
+                (
+                    new User("Lukski175", "113596146+Lukski175@users.noreply.github.com"),
+                    new List<DateCount>()
+                    {
+                        new DateCount(new DateTime(2022, 10, 4), 1),
+                    }
+                ),
+                new UserDateCounts
+                (
+                    new User("Moedefeis", "71703802+Moedefeis@users.noreply.github.com"),
+                    new List<DateCount>()
+                    {
+                        new DateCount(new DateTime(2022, 10, 4), 2),
+                    }
+                ),
+                new UserDateCounts
+                (
+                    new User("Mathias Hvolgaard Darling Larsen", "mhvl@itu.dk"),
+                    new List<DateCount>()
+                    {
+                        new DateCount(new DateTime(2022, 10, 4), 3),
+                    }
+                ),
+                new UserDateCounts
+                (
+                    new User("lukas175", "79913864+lukas175@users.noreply.github.com"),
+                    new List<DateCount>()
+                    {
+                        new DateCount(new DateTime(2022, 10, 4), 3),
+                    }
+                ),
             };
 
-            var result = await _sut.Get(_gitOwner, _gitRepositoryName, _gitUser);
-            result.Should().BeEquivalentTo(expected);
+            // Just take first 4
+            var result = await _controller.Get(_gitOwner, _gitRepositoryName, "user");
+            result.Take(4).Should().BeEquivalentTo(expected);
         }
-
-
     }
 }
