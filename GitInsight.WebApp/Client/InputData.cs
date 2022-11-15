ce78b3232b588f37f41f779d19d4d01119f09b81
Adrian Kari <adrian@kari.dk>
2022-11-15 14:50:30 +0100

    Forks that dont work

--- GitInsight.WebApp/Client/InputData.cs
+++ GitInsight.WebApp/Client/InputData.cs
@@ -2,7 +2,7 @@
 {
     public static class InputData
     {
-        public static string NameOfOwner { get; set; } = "Kliaxe";
-        public static string NameOfRepository { get; set; } = "GitInsight";
+        public static string NameOfOwner { get; set; } = "itu-bdsa";
+        public static string NameOfRepository { get; set; } = "project-description";
     }
 }
